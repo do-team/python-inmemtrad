fd90f069fd073406e5db17b6f6f1d3f5a577a284
Administrator <admin@example.com>
2016-12-03 16:44:14 +0100

    Various updates in pgm structure

--- inmemtrad.py
+++ inmemtrad.py
@@ -1,16 +1,18 @@
 # /usr/bin/python
 '''
- Purpose : Generate random product and it's price
+ Purpose : Simple program to generate random product and it's price
+ Format  : <prod1>-<price>-hash: customer
 '''
 
 import random
 import redis
+import time
 
 POOL = redis.ConnectionPool(host='mycentos7', port=6379, db=0)
 
-customers = ["AUX1", "BFG2", "EFI3", "DRS4", "FRA5"]
-products = ["wood", "gold", "stone", "food"]
-variable_cycles = 500
+customers = ["AUX1", "BFG2", "CRE3", "DRS4", "EFI5", "FRA6", "GOR1", "AAR1"]
+products = ["wood", "gold", "fish", "wine"]
+variable_cycles = 100
 
 
 def get_variable(variable_name):
@@ -19,22 +21,46 @@ def get_variable(variable_name):
     return response
 
 
-def set_variable(variable_name, variable_value):
+def set_variable(variable_name, variable_value, variable_expire):
     r = redis.Redis(connection_pool=POOL)
-    r.mset(variable_name, variable_value, 2, 1)
+    r.set(variable_name, variable_value, variable_expire)
 
 
-def generateOrders(cycles):
-    for i in range(cycles):
-        product = str(random.choice(customers)) + " " + str(random.choice(products))
-        value   = str(random.randint(1,100))
+def generate_orders(cycles=1, expire=1):
+    for cycle in range(cycles):
+        item, customer = randomize_values()
+        print "Company " + str(customer) + " offers : " + str(item[0:4]) + " for " + str(item[5:10]) + " ( " + item + " )"
+        set_variable(item, customer, expire)
+    return
 
-        print "Generating random price for : " + str(i) + " " + product + " " + value
-        set_variable(product, value)
+
+def randomize_values():
+    item = str(random.choice(products)) + "-" + str(random.randint(1,1000))
+    customer = str(random.choice(customers))
+    return item, customer
+
+
+def test_case_buy():
+    item_test = (randomize_values())
+    find_item = get_variable(item_test[0])
+    my_uid = item_test[1]
+    print "BUYER ID: " + my_uid + "   Looking for item : " + str(item_test[0])
+
+    if find_item:
+        print "There's offer for >>> " + str(item_test[0]) + " from : " +str(find_item)
+    else:
+        print "Nothing found, sorry"
 
 if __name__ == '__main__':
     print('===============================================================')
     print('Generating orders for Redis')
     print('===============================================================')
-    generateOrders(variable_cycles)
-    print "Getting latest price for WOOD: " + str(get_variable('FRA5 wood'))
+    generate_orders(variable_cycles, expire=2)
+
+    print('===============================================================')
+    print('Time for tests')
+    print('===============================================================')
+    time.sleep(2)
+    test_case_buy()
+
+
